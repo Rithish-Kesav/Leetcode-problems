d16a79fe80b72423646a4c58fa516a50ff19db0d
Rithish Kesav Saravanan <rithish_saravanan@mohawkind.com>
2022-09-13 16:57:44 -0700

    Final push

--- 461. Hamming Distance/solution.py
+++ 461. Hamming Distance/solution.py
@@ -11,11 +11,11 @@ class Solution:
     
     #Second solution
     def hammingDistance(self, x: int, y: int) -> int:
-        cnt = 0 
+        res = 0 
         while x or y: 
             if (x&1) ^ (y&1): 
                 cnt += 1 
             x >>= 1 
             y >>= 1 
-        return cnt
+        return res
     
