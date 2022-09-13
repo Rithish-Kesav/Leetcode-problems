4b81a5143f7df2924d1c84e2fa9eca08bbbba9d5
Rithish Kesav Saravanan <rithish_saravanan@mohawkind.com>
2022-09-12 23:55:35 -0700

    With secondary solution

--- 283. Move Zeroes/solution.py
+++ 283. Move Zeroes/solution.py
@@ -3,19 +3,26 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
+        #My solution
+        
         l = 0
         for r in range(len(nums)):
             if nums[l] != 0 and nums[r] != 0:
                 l+=1
-                r+=1
-            elif nums[l] == 0 and nums[r] == 0:
-                r+=1
             elif nums[l] == 0 and nums[r] != 0:
                 nums[l],nums[r] = nums[r],nums[l]
                 l+=1
-                r+=1
             elif len(nums) == 1:
                 return nums
         return nums
+        
+        #Secondary solution
+        l = 0
+        for r in range(len(nums)):
+            if nums[r]:
+                nums[l],nums[r] = nums[r],nums[l]
+                l+=1
+        return nums
+        
                 
             
