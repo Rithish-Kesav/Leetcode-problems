b7c772521cec3e2d872ae965436f86c52d36cc60
Rithish Kesav Saravanan <rithish_saravanan@mohawkind.com>
2022-10-23 13:06:05 -0700

    Neetcode solution

--- 19. Remove nth node from end of list/solution.py
+++ 19. Remove nth node from end of list/solution.py
@@ -23,3 +23,24 @@ class Solution:
             count2 += 1
         itr = head
         return itr
+
+# Another solution
+
+
+class Solution:
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        dummy = ListNode(0, head)
+        left = dummy
+        right = head
+
+        while n > 0 and right:
+            right = right.next
+            n -= 1
+
+        while right:
+            left = left.next
+            right = right.next
+
+        left.next = left.next.next
+
+        return dummy.next
