b7e6c4c3b3adf5202edb2ce36a15bcfe7e6f108a
Rithish Kesav Saravanan <rithish_saravanan@mohawkind.com>
2022-10-25 18:36:12 -0700

    Solution 2 for buy and sell stocks

--- 122. Best Time to Buy and Sell Stock II/solution.py
+++ 122. Best Time to Buy and Sell Stock II/solution.py
@@ -12,3 +12,14 @@ class Solution:
             return sum(diff)
         else:
             return 0
+
+# Solution 2
+
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        profit = 0
+        for i in range(1, len(prices)):
+            if prices[i] > prices[i-1]:
+                profit += prices[i] - prices[i-1]
+        return profit
