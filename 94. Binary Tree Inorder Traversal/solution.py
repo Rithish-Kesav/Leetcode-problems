86589bfc389ae2205fd7b8fe7c0407834237d6f1
Rithish Kesav Saravanan <rithish_saravanan@mohawkind.com>
2022-11-13 13:11:40 -0700

    2 other solutions to 94th problem

--- 94. Binary Tree Inorder Traversal/solution.py
+++ 94. Binary Tree Inorder Traversal/solution.py
@@ -18,3 +18,37 @@ class Solution:
             elements += self.inorderTraversal(root.right)
 
         return elements
+
+# Alternate recursive solution
+
+
+class Solution:
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        elements = []
+
+        def inorder(root):
+            if not root:
+                return
+            inorder(root.left)
+            elements.append(root.val)
+            inorder(root.right)
+        inorder(root)
+        return elements
+
+# Iterative solution
+
+
+class Solution:
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        elements = []
+        cur = root
+        stack = []
+
+        while cur or stack:
+            while cur:
+                stack.append(cur)
+                cur = cur.left
+            cur = stack.pop()
+            elements.append(cur.val)
+            cur = cur.right
+        return elements
