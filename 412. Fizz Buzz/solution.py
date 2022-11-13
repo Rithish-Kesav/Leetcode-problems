cc5fc2c447c628acdd2595795249ca5c47db069a
Rithish Kesav Saravanan <rithish_saravanan@mohawkind.com>
2022-11-13 15:03:18 -0700

    Test push

--- 412. Fizz Buzz/solution.py
+++ 412. Fizz Buzz/solution.py
@@ -1,14 +1,14 @@
+# Test push
 class Solution:
     def fizzBuzz(self, n: int) -> List[str]:
         answer = []
-        for i in range(1,n+1):
-            if i%3==0 and i%5==0:
+        for i in range(1, n+1):
+            if i % 3 == 0 and i % 5 == 0:
                 answer.append("FizzBuzz")
-            elif i%3 ==0 and i%5!=0:
+            elif i % 3 == 0 and i % 5 != 0:
                 answer.append("Fizz")
-            elif i%5 ==0 and i%3!=0:
+            elif i % 5 == 0 and i % 3 != 0:
                 answer.append("Buzz")
-            elif i%3!=0 and i%5!=0:
+            elif i % 3 != 0 and i % 5 != 0:
                 answer.append(str(i))
         return answer
-            
