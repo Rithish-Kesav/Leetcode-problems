f2b7fbb161b40813e78d1dec6f5a3b74ac3e307b
Rithish Kesav Saravanan <rithish_saravanan@mohawkind.com>
2022-12-19 17:33:48 -0700

    Solution 2

--- 169. Majority Element/solution.py
+++ 169. Majority Element/solution.py
@@ -4,3 +4,19 @@ class Solution:
         for key, val in hashMap.items():
             if val > len(nums)/2:
                 return key
+
+
+# solution 2 - creating hashMap without Counter
+
+
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        hashMap = {}
+        for n in nums:
+            if n not in hashMap:
+                hashMap[n] = 1
+            else:
+                hashMap[n] += 1
+        for key, val in hashMap.items():
+            if val > len(nums)/2:
+                return key
