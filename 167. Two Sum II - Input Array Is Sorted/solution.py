256eb666d63c122a95d6a28f51f9d2fdb0652920
Rithish Kesav Saravanan <rithish_saravanan@mohawkind.com>
2023-04-06 09:05:45 -0700

    Neetcode 2 pointer solution

--- 167. Two Sum II - Input Array Is Sorted/solution.py
+++ 167. Two Sum II - Input Array Is Sorted/solution.py
@@ -8,3 +8,18 @@ class Solution:
                 return ([hMap[diff], i+1])
             hMap[n] = i+1
         return
+
+# Neetcode solution
+
+
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        left, right = 0, len(numbers)-1
+        while left < right:
+            if numbers[left]+numbers[right] > target:
+                right -= 1
+            elif numbers[left]+numbers[right] < target:
+                left += 1
+            else:
+                return [left+1, right+1]
+                break
